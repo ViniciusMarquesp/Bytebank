b39d1df8a0bee530bac1411ecc4a9bf823293bb8
Vinicius Marques <ciusivinicius@hotmail.com>
2023-01-23 15:02:53 -0300

    bytebank start

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Vinicius"
+    val numeroConta: Int = 1234
+    var saldo: Double = 0.0
+    saldo = 102.0
+    saldo += 200
+
+    println("titular: $titular")
+    println("conta: $numeroConta")
+    println("saldo: $saldo")
+}
